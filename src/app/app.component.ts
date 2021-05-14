38e869c2188cc0f28d702dfbb5f24262db7c312b
rohjain2 <rohjain2@cisco.com>
2021-05-13 20:51:10 -0700

    Adding resume download capability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ResumeRohitJain';
+
+  downloadResume() {
+    let link = document.createElement('a');
+    link.setAttribute('type', 'hidden');
+    link.href = 'assets/RohitResume.pdf';
+    link.download = "RohitJainResume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
 }
